ba3eb75d3cdfb2597cdb8881b09cabf2c3461e62
Marcello Sega <marcello.sega@gmail.com>
2017-03-09 16:36:28 +0100

    update

--- observables-1.py
+++ observables-1.py
@@ -9,7 +9,7 @@ oxygens = u.select_atoms("name OW")
 radii=pytim_data.vdwradii(G43A1_TOP)
 interface = pytim.ITIM(u,alpha=2.,itim_group=oxygens,max_layers=4,multiproc=True,radii_dict=radii,cluster_cut=3.5)
 for ts in u.trajectory[::5] :
-    layer=interface.layers('upper',1)
+    layer=interface.layers[0,1]
     if ts.frame==0 :
         rdf=pytim.observables.InterRDF2D(layer,layer,range=(0.,L/2.),nbins=120)
     rdf.sample(ts)
