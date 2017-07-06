4c279c49edd88495ecdfc9623ac6e3584ee16a3f
Marcello Sega <marcello.sega@gmail.com>
2017-07-06 20:12:18 +0200

    update

--- observables-1.py
+++ observables-1.py
@@ -4,14 +4,16 @@ import pytim
 import matplotlib.pyplot as plt
 from   pytim.datafiles import *
 u = mda.Universe(WATER_GRO,WATER_XTC)
-L = np.min(u.dimensions[:3])
 oxygens = u.select_atoms("name OW")
-radii=pytim_data.vdwradii(G43A1_TOP)
-interface = pytim.ITIM(u,alpha=2.,itim_group=oxygens,                               max_layers=4,radii_dict=radii,                               cluster_cut=3.5)
-rdf=pytim.observables.RDF2D(u,nbins=120)
-for ts in u.trajectory[::50] :
-    layer=interface.layers[0,1]
+interface = pytim.ITIM(u,alpha=2.,group=oxygens,                               cluster_cut=3.5, molecular=False)
+rdf=pytim.observables.RDF2D(u,nbins=250)
+for ts in u.trajectory[::] :
+    layer=interface.layers[0,0]
     rdf.sample(layer,layer)
-rdf.rdf[0]=0.0
+rdf.count[0]=0
+
 plt.plot(rdf.bins, rdf.rdf)
+
+plt.gca().set_xlim([0,7])
+
 plt.show()
--- observables-2.py
+++ observables-2.py
@@ -3,17 +3,18 @@ import numpy as np
 import pytim
 import matplotlib.pyplot as plt
 from   pytim.datafiles   import *
+from pytim.observables import Profile,Number
 
 u       = mda.Universe(WATER_GRO,WATER_XTC)
 oxygens = u.select_atoms("name OW")
-radii=pytim_data.vdwradii(G43A1_TOP)
 
-obs     = pytim.observables.Number()
-profile = pytim.observables.Profile(group=oxygens,observable=obs)
+obs     = Number()
 
-interface = pytim.ITIM(u, alpha=2.0, max_layers=1,cluster_cut=3.5)
+interface = pytim.ITIM(u, alpha=2.0, max_layers=4,cluster_cut=3.5,centered=True,molecular=False)
 
-for ts in u.trajectory[::50]:
+profile = Profile(group=oxygens,observable=obs)
+
+for ts in u.trajectory[::]:
     profile.sample()
 
 low, up, avg = profile.get_values(binwidth=1.0)
